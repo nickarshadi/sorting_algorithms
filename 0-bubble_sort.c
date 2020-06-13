33a68b91208c467571a399f5af751f87f5734222
nickarshadi <1477@holbertonschool.com>
2020-06-13 18:43:36 -0500

    Create bubble sort alghorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array in bubble sort
+ * print the array after every swap
+ * @array: unsorted array
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, temp = 0;
+
+	for (; i <= size; i++)
+	{
+		for (; j <= size - 1; j++)
+		{
+			if (array[j] > array[j + 1] && (j + 1) <= size)
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		j = 0;
+	}
+}
