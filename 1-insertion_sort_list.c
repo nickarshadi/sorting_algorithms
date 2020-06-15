aa69271e22447ad9075434edbcc51fe118b3e926
nickarshadi <1477@holbertonschool.com>
2020-06-15 10:16:11 -0500

    Fix insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,49 @@
 #include "sort.h"
 
 /**
- * swap - swaps 2 nodes in a doubly-linked list
- * @a: address of first node
- * @b: address of second node
- *
- * Return: void
+ * swap - swap two nodes of a foubly linked list
+ * @a: address of node1(prev)
+ * @b: address of node2
  */
 void swap(listint_t *a, listint_t *b)
 {
-        if (a->prev)
-                a->prev->next = b;
-        if (b->next)
-                b->next->prev = a;
-        a->next = b->next;
-        b->prev = a->prev;
-        a->prev = b;
-        b->next = a;
-
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
 
 /**
  * insertion_sort_list - insertion sorts a doubly-linked list
  * @list: address of pointer to head node
- *
- * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *i, *j;
+	listint_t *node = NULL, *pr = NULL;
 
-        if (!list || !*list || !(*list)->next)
-                return;
-        i = (*list)->next;
-        while (i)
-        {
-                j = i;
-                i = i->next;
-                while (j && j->prev)
-                {
-                        if (j->prev->n > j->n)
-                        {
-                                swap(j->prev, j);
-                                if (!j->prev)
-                                        *list = j;
-                                print_list((const listint_t *)*list);
-                        }
-                        else
-                                j = j->prev;
-                }
+	if (!list || !*list || !(*list)->next)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		pr = node;
+		node = node->next;
+		while (pr && pr->prev)
+		{
+			if (pr->prev->n > pr->n)
+			{
+				swap(pr->prev, pr);
+				if (!pr->prev)
+					*list = pr;
+				print_list((const listint_t *)*list);
+			}
+			else
+				pr = pr->prev;
+
 		}
+	}
 }
