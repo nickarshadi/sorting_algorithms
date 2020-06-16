16745550672c0c2638182d40db5686e6b5670440
nickarshadi <1477@holbertonschool.com>
2020-06-16 15:50:23 -0500

    Add quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sort array via quicksort
+ * @array: pointer to int array
+ * @size: amounts of elements in array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+}
